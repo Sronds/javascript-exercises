b207b8e01ccddffb484c872509482972bb5f74f1
Sronds <srondyyoussef@gmail.com>
2021-12-23 18:26:13 +0100

    Finished first exercise JS (took forever)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, number) { 
+    let repeatedString = ""
+        while (number > 0) {
+            repeatedString += string;
+            number--
+           } if (number < 0) {
+               return "ERROR"
+           }
+    return repeatedString;
 };
+
+// Step 1) Type in text
+// Step 2) When text is typed, repeat text 'n' times. 
 
 // Do not edit below this line
 module.exports = repeatString;
