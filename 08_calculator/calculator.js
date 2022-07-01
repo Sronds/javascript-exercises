cf5d73a2b99984d6e1e5ba6808cbcd2011049f04
Youssef Srondy <srondyyoussef@gmail.com>
2022-07-01 14:07:42 +0200

    calculator problems solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return (x + y)
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return (x - y)
 };
 
-const sum = function() {
-	
+/* 
+Remember, when calculating totals, you want to initialise
+your "total" variable outside of your for loop, and 
+execute your return function outside of the for loop 
+as well, or else it will only run once.
+*/
+
+const sum = function(array) {
+  total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i] 
+  } 
+  return total 
 };
 
-const multiply = function() {
 
+
+
+const multiply = function(array) {
+  total = 1;
+  for (i = 0; i < array.length; i++) {
+    total *= array[i] 
+  } 
+  return total 
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  return (x ** y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  total = 0
+  if (x == 0) {
+    return 1 
+  } else {
+    total += x * factorial(x - 1)
+  }
+  return total  
 };
+
 
 // Do not edit below this line
 module.exports = {
